1a2081084bf0ff7b9262cc3bef4a359ea0727aaf
JackWu-SiHao <shwu.cs98@gmail.com>
2013-07-24 22:23:29 +0800

    Add py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
+class MyClassHandler(object):
+	"""docstring for MyClassHandler"""
+	def __init__(self, arg):
+		super(MyClassHandler, self).__init__()
+		self.arg = arg
 
-print "Hello World"
-print "test branch conflict"
-
-a = 1
-print "Jack"
-print "Haley"
---
+++ test2.py
@@ -0,0 +1,13 @@
+
+
+class MainHandler(object):
+	"""docstring for MainHandler"""
+	def __init__(self, arg):
+		super(MainHandler, self).__init__()
+		self.arg = arg
+
+	def function():
+		pass
+
+	def functools():
+		pass
